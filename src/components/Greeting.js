3d26b256cbedd994005c2a08e833d030beaa36c7
katreddyvenkateswaraReddy <katreddy41@gmail.com>
2023-12-13 15:01:36 +0530

    New

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -9,9 +9,11 @@ const [text,setNewText] = useState("");
         <div>
         <form>
             <label for="text">Enter your name:</label>
-            <input type="text" id="text" onChange={e=>setNewText(e.target.value)}/>
+            <br/>
+            <input type="text" id="text" onChange={e=>setNewText(e.target.value)} value={text}/>
         </form>
-        {text?<p>Hello {text}!</p>:""}
+        {/* {text?<p>Hello {text}!</p>:""} */}
+        <p>Hello {text}!</p>
         </div>
     )
 }
